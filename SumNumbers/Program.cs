5e3564c0cb2bbf52ad80dbbbba8210855e633dd1
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 21:12:23 +0300

    Initial commit

---
+++ SumNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿class SumNumbers {
+    public static void Main(string[]args)
+    {
+
+        int n = int.Parse(Console.ReadLine()); 
+        double sum=0;
+
+        for(int i=0;i<n;i++)
+        {
+            double num = double.Parse(Console.ReadLine());
+            sum += num;
+        }
+        Console.WriteLine(sum);
+    }
+}
